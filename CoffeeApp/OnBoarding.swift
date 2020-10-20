a2edb5918642f6076dc7b88352a65b21941063eb
Dmitriy Borisov <dm.borisov1982@gmail.com>
2020-10-21 01:15:06 +0300

    Добавил дизайн экранов загрузки приложения и экрана логина

--- CoffeeApp/OnBoarding.swift
+++ CoffeeApp/OnBoarding.swift
@@ -9,7 +9,56 @@ import SwiftUI
 
 struct OnBoarding: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30) {
+            Image("artwork")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.bottom, 50)
+            Text("Самый лучший кофе\n только у нас")
+                .font(.system(size: 32)).bold().italic()
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color("fontcolor"))
+            HStack(spacing: 30) {
+                Button(action: {
+                    
+                }) {
+                    Text("Регистрация")
+                }
+                .padding(15)
+                .padding(.horizontal, 10)
+                .background(Color("buttoncolor"))
+                .foregroundColor(.white)
+                .cornerRadius(30)
+                Button(action: {
+                    
+                }) {
+                    Text("Войти")
+                }
+                .padding(15)
+                .padding(.horizontal, 10)
+                .foregroundColor(Color("buttoncolor"))
+                .cornerRadius(30)
+                .overlay(
+                            RoundedRectangle(cornerRadius: 30)
+                                .stroke(Color("buttoncolor"), lineWidth: 2))
+            }
+            
+            Button(action: {
+                
+            }) {
+                Image("facebook")
+                Text("Зайти по FaceBook")
+            }
+            .padding(15)
+            .padding(.horizontal, 40)
+            .foregroundColor(Color(#colorLiteral(red: 0.2588235294, green: 0.4666666667, blue: 0.737254902, alpha: 1)))
+            .cornerRadius(30)
+            .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(Color(#colorLiteral(red: 0.2588235294, green: 0.4666666667, blue: 0.737254902, alpha: 1)), lineWidth: 2))
+            Spacer()
+            
+        }
     }
 }
 
