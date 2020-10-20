ec5874fc4ec5566259746bcd73c63f2cc45b5eac
Dmitriy Borisov <dm.borisov1982@gmail.com>
2020-10-20 23:32:22 +0300

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Dmitriy Borisov on 20.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
