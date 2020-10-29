3ae2af1b39f2e4301e5635013dbe2df7137d5327
Dmitriy Borisov <dm.borisov1982@gmail.com>
2020-10-30 01:45:14 +0300

    Поправил карту, но поинтер все равно не появился. Так же не совсем понятно, нахуя там продублировано все. несколько строк сверху и потом их же дублируют

--- CoffeeApp/MapViewPage.swift
+++ CoffeeApp/MapViewPage.swift
@@ -11,7 +11,7 @@ import MapKit
 struct MapViewPage: View {
     @State var coordinateRegion = MKCoordinateRegion(
           center: CLLocationCoordinate2D(latitude: 55.707030, longitude: 37.592209),
-          span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+          span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
         
         var body: some View {
           Map(coordinateRegion: $coordinateRegion)
@@ -25,8 +25,8 @@ struct MapViewPage: View {
         ]
         
         @State var coordinateRegion = MKCoordinateRegion(
-          center: CLLocationCoordinate2D(latitude: 56.948889, longitude: 24.106389),
-          span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+          center: CLLocationCoordinate2D(latitude: 55.707030, longitude: 37.592209),
+          span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
         
         var body: some View {
           Map(coordinateRegion: $coordinateRegion,
