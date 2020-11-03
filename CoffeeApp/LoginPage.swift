63764699cb2f8d69df8bfceacb2a8552347fc42d
Dimon <tarakan1982@gmail.com>
2020-11-03 16:54:21 +0300

    Убрал все что говорили в уроке по взаимодействию с базой. Вечером сделаю заново

--- CoffeeApp/LoginPage.swift
+++ CoffeeApp/LoginPage.swift
@@ -10,13 +10,34 @@ import Firebase
 import FirebaseAuth
 
 struct LoginPage: View {
+    
+    var body: some View {
+        DesignLoginWindow()
+    }
+}
+
+struct HomeMessage: View {
+    var body: some View {
+        VStack {
+            Text("Home")
+            Button(action: {
+                UserDefaults.standard.set(false, forKey: "status")
+                NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
+            }) {
+                Text("Logout")
+            }
+        }
+    }
+}
+
+struct DesignLoginWindow: View {
     @State var emailTextField: String = ""
     @State var passwordTextField: String = ""
     @State var message = ""
     @State var alert = false
     @State var show = false
     var body: some View {
-        NavigationView {
+        VStack {
             VStack {
                 HStack {
                     Text("С возвращением!")
@@ -52,15 +73,7 @@ struct LoginPage: View {
                     }
                 } .padding(.bottom, 24)
                 Button(action: {
-                    signInWithEmail(email: self.emailTextField, password: self.passwordTextField) { (verified, status) in
-                        if !verified {
-                            self.message = status
-                            self.alert.toggle()
-                        } else {
-                            UserDefaults.standard.set(true, forKey: "status")
-                            NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
-                        }
-                    }
+                    
                 }) {
                     Text("Войти")
                 }
@@ -70,53 +83,26 @@ struct LoginPage: View {
                 .background(Color("buttoncolor"))
                 .foregroundColor(.white)
                 .cornerRadius(30)
-                .alert(isPresented: $alert) {
-                    Alert(title: Text("Error"), message: Text(self.message), dismissButton: .default(Text("OK")))
+                
                 }
                 HStack {
                     Text("До сих пор нет аккаунта?")
                         .foregroundColor(Color(#colorLiteral(red: 0.2431372549, green: 0.2901960784, blue: 0.3490196078, alpha: 1)).opacity(0.45))
                     Button(action: {
-                        signUpWithEmail(email: self.emailTextField, password: self.passwordTextField) { (verified, status) in
-                            if !verified {
-                                self.message = status
-                                self.alert.toggle()
-                            } else {
-                                UserDefaults.standard.set(true, forKey: "status")
-                                NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
-                            }
-                        }
+                        
                     }) {
                         Text("Регистрация")
                             .foregroundColor(Color("buttoncolor"))
                     }
-                    .sheet(isPresented: $show) {
-                        LoginPage()
-                    }
+                    
                 }
                 Spacer()
                 }
             .padding(.horizontal)
             .navigationBarTitle("Вход")//Ну к этому есть ряд вопросов: как поменять цвет, как поднять выше. Можно попробовать без Навигейшена вовсе обойтись и тогда будет все гораздо проще, но на следующих экранам мне это сильно аукнется
             
-        }
     }
 }
-
-struct HomeMessage: View {
-    var body: some View {
-        VStack {
-            Text("Home")
-            Button(action: {
-                UserDefaults.standard.set(false, forKey: "status")
-                NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
-            }) {
-                Text("Logout")
-            }
-        }
-    }
-}
-
 func signInWithEmail(email: String, password: String, completion: @escaping (Bool, String) -> Void) {
     Auth.auth().signIn(withEmail: email, password: password) { (res, err) in
         if err != nil {
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CoffeeApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CoffeeApp
+# add pods for desired Firebase products
+pod 'Firebase/Analytics'
+pod 'Firebase/Auth'
+# https://firebase.google.com/docs/ios/setup#available-pods
+
+end
